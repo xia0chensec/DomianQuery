4d9e125ba5b1dc3b8133a0619c4d23c6a15f885c
xia0chen <145250613+xla0Chen@users.noreply.github.com>
2023-10-04 12:01:31 +0800

    Update ipsearch.py

--- ipsearch/ipsearch.py
+++ ipsearch/ipsearch.py
@@ -6,8 +6,6 @@ import tldextract
 import requests
 from module.baidu import query_domain_info
 import time
-
-
 def query_ip_info(ipaddress, output_file):
     mainDomainNameList = []
 
@@ -16,44 +14,23 @@ def query_ip_info(ipaddress, output_file):
     url = f'https://api.webscan.cc/?action=query&ip={ipaddress}'
     try:
         rep = requests.get(url=url, headers=headers, timeout=2)
-        if rep.text == "null":
-            print(f"IP->[{ipaddress}]未反查到域名")
-            print("\033[0m")
-        else:
-            results = rep.json()
+        results = rep.json()  # 解析API响应
+
+        if results and results[0]["domain"] != ipaddress:
             for result in results:
                 domainName = result["domain"]
-                if domainName == ipaddress:
-                    print(f"IP->[{ipaddress}]未反查到域名")
-                    print("\033[0m")
-                else:
-                    if re.match(
-                            r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",domainName):
-                        continue
-                    # print(domainName)
-                    if domainName in mainDomainNameList:
-                        continue
- 
-                    val = tldextract.extract(domainName)
-                    domain = f"{val.domain}.{val.suffix}"
-
+                if re.match(
+                        r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
+                        domainName):
+                    continue
+                val = tldextract.extract(domainName)
+                domain = f"{val.domain}.{val.suffix}"      
             print(f"IP->[{ipaddress}]反查到的域名为: {domain}")
-            domain_info=query_domain_info(domain,output_file=output_file)
-            #print("主域名:", domain)
-        
-    except:
+            query_domain_info(domain, output_file=output_file)  # 查询域名信息
+        else:
+            print(f"IP->[{ipaddress}]未反查到域名")
+            print("\033[0m")
+    except Exception as e:
         pass
 
-def query_batch_ips(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            ip_list = [line.strip() for line in file]
-
-        for ip in ip_list:
-            query_ip_info(ip)
-
-    except FileNotFoundError:
-        print(f"文件 {file_path} 不存在")
-    except Exception as e:
-        print(f"发生未知错误: {e}")
 
