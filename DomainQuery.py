e73f735f86b4faca5191eb097508af74f4929e61
xia0chen <145250613+xla0Chen@users.noreply.github.com>
2023-10-04 12:00:29 +0800

    Update DomainQuery.py

--- DomainQuery.py
+++ DomainQuery.py
@@ -7,12 +7,33 @@ from module.log import banner
 from ipsearch.ipsearch import query_ip_info
 import ipaddress
 import requests
+import re
+import tldextract
+from urllib.parse import urlparse
 requests.packages.urllib3.disable_warnings()  # 抑制https错误信息
-def is_valid_ip(ip):
-    try:
-        ipaddress.ip_address(ip)
-        return True
-    except ValueError:
+
+def extract_and_validate_ip(ip):
+    ip_pattern = r'(\d+\.\d+\.\d+\.\d+)'  # 正则表达式用于提取IPv4地址
+    match = re.search(ip_pattern, ip)
+    
+    if match:
+        extracted_ip = match.group(1)  # 提取第一个分组中的IP地址
+        return extracted_ip
+    else:
+        # 提取域名，例如http://bbfx.scuvc.com,则取出域名scuvc.com
+        extracted_info = tldextract.extract(ip)
+        top_level_domain = extracted_info.domain + "." + extracted_info.suffix
+        return top_level_domain
+
+def is_valid_ip(ip):     #判断输入是否合法
+    extracted_ip = extract_and_validate_ip(ip)
+    if extracted_ip:
+        try:
+            ipaddress.ip_address(extracted_ip)
+            return True
+        except ValueError:
+            return False
+    else:
         return False
 
 def main():
@@ -20,34 +41,36 @@ def main():
     args = parseClass.parse_args()
     output_file=args.output
     if args.url:
-        # 判断输入是否为IP地址
-        if is_valid_ip(args.url):
-            # 进行IP域名反查
-            domain = query_ip_info(args.url,output_file=output_file)
-            # 再进行域名权重查询
-            query_domain_info(domain, args.output)
-        else:
-            # 直接进行域名权重查询
-            query_domain_info(args.url, args.output)
+        extracted_info = extract_and_validate_ip(args.url)
+        if extracted_info:
+            if is_valid_ip(extracted_info):
+                #如果是IP则进行IP反查
+                domain=query_ip_info(extracted_info,output_file=output_file)
+                #query_domain_info(domain, args.output)
+            else:
+                domain=extracted_info
+            #如果是域名则进行权重查询
+            query_domain_info(domain,args.output)
     elif args.file:
         try:
             with open(args.file, "r") as file:
                 input_list = [line.strip() for line in file]
 
             for input_item in input_list:
-                if is_valid_ip(input_item):
-                    # 进行IP域名反查
-                    domain = query_ip_info(input_item,output_file=output_file)
+                extracted_info=extract_and_validate_ip(input_item)
+                if is_valid_ip(extracted_info):
+                    #提取的是IP，进行IP域名反查
+                    domain=query_ip_info(extracted_info,output_file=output_file)
                     # 再进行域名权重查询
                     query_domain_info(domain, args.output)
                 else:
-                    # 直接进行域名权重查询
-                    query_domain_info(input_item, args.output)
-
+                    domain=extracted_info
+                #如果是域名则进行权重查询
+                query_domain_info(domain, args.output)
         except FileNotFoundError:
             print(f"找不到文件 {args.file}")
     else:
-        print("请提供合适的参数：-u 域名或IP地址，-f 批量查询文件路径")
+        print("Example:\npython DomainQuery.py -u ip/domain\npython DomainQuery.py -f file.txt\npython DomainQuery.py -f file.txt -o result.txt")
 
 if __name__ == "__main__":
     banner()
